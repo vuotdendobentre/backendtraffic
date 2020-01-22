b683813885ced9039a78d8cfebe8659a1260b7b2
lcnghia95 <lcnghia95@gmail.com>
2020-01-22 22:47:00 +0700

    update

--- api/routes/failRoute.js
+++ api/routes/failRoute.js
@@ -1,22 +1,22 @@
 module.exports = function(app){
     var Fail = require('../controllers/failController');
 
-    //Fail route
+    // Fail route
 
-    // app.route('/fails/newbydate/:plate/:date/:time')
-    //     .get(Fail.read_list_onlydate)
+    app.route('/fails/newbydate/:plate/:date/:time')
+        .get(Fail.read_list_onlydate)
 
-    // app.route('/fails')     
-    //     .get(Fail.read_list_fail)      
-    //     .post(Fail.create_a_fail)
+    app.route('/fails')     
+        .get(Fail.read_list_fail)      
+        .post(Fail.create_a_fail)
 
-    // app.route('/fails/:failname')
-    //     .get(Fail.read_a_fail)
+    app.route('/fails/:failname')
+        .get(Fail.read_a_fail)
         
-    // app.route('/newfails')
-    //     .post(Fail.create_new_fail)
+    app.route('/newfails')
+        .post(Fail.create_new_fail)
         
-    // app.route('/superadmin')
-    //     .post(Fail.all_submit)
+    app.route('/superadmin')
+        .post(Fail.all_submit)
    
 };
